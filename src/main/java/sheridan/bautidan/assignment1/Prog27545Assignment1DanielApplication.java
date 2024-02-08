b51555f03f5684f8a8a97a8fe2347466ef57fa2f
daanino <bautidan@sheridancollege.ca>
2024-02-07 23:25:52 -0500

    Initial commit.

---
+++ src/main/java/sheridan/bautidan/assignment1/Prog27545Assignment1DanielApplication.java
@@ -0,0 +1,13 @@
+package sheridan.bautidan.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog27545Assignment1DanielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog27545Assignment1DanielApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/bautidan/assignment1/Prog27545Assignment1DanielApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.bautidan.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog27545Assignment1DanielApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
